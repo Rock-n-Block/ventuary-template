fb24685682edca36d278bed775a73ba81a619a42
lamiqv <laumiqv@gmail.com>
2020-05-15 15:54:05 +0300

    news block (four), clean second block style

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,16 @@
 import $ from '../plugins/jquery/dist/jquery.min';
 
+var menuToggle = false;
+
 $(document).ready(() => {
-    // eslint-disable-next-line no-console
-    console.log(`document ready`)
-})
+    console.log('ready')
+    $('.header-menu-toggle').click(function () {
+        console.log('toggle menu')
+        if (!menuToggle) $('.header-wrap').addClass('header-menu-show');
+        if (menuToggle) $('.header-wrap').removeClass('header-menu-show');
+    });
+});
+
+$(document).scroll(() => {
+    window.scrollY > 60 ? ($('header').addClass('header-fixed')) : ($('header').removeClass('header-fixed'));
+});
