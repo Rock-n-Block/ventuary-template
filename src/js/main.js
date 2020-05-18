dbf2a524bb38ab0f17d83efbe792a35346b5f54f
lamiqv <laumiqv@gmail.com>
2020-05-18 18:32:15 +0300

    changes, add links to blocks

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,34 @@
-import $ from '../plugins/jquery/dist/jquery.min';
+import $ from '../plugins/jquery/dist/jquery.min.js';
 
 var menuToggle = false;
 
 $(document).ready(() => {
-    console.log('ready')
     $('.header-menu-toggle').click(function () {
         console.log('toggle menu')
         if (!menuToggle) $('.header-wrap').addClass('header-menu-show');
         if (menuToggle) $('.header-wrap').removeClass('header-menu-show');
     });
+
+    // Array.from(document.getElementsByClassName("section")).forEach(
+    //     function (element, index, array) {
+    //         element.style.minHeight = element.children[0].clientHeight + 'px';
+    //     }
+    // );
+
 });
 
 $(document).scroll(() => {
     window.scrollY > 60 ? ($('header').addClass('header-fixed')) : ($('header').removeClass('header-fixed'));
+    // parallaxScroll();
 });
+
+// function parallaxScroll() {
+//     const scrolled = $(window).scrollTop();
+//     $('#header').css('top', (0 - (scrolled * .10)) + 'px');
+//     $('#first').css('top', (0 - (scrolled * .10)) + 'px');
+//     $('#second').css('top', (0 - (scrolled * .20)) + 'px');
+//     $('#third').css('top', (0 - (scrolled * .30)) + 'px');
+//     $('#four').css('top', (0 - (scrolled * .40)) + 'px');
+//     $('#five').css('top', (0 - (scrolled * .50)) + 'px');
+//     $('#six').css('top', (0 - (scrolled * .60)) + 'px');
+// }
